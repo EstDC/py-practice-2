59e648b8d75d36c6190a4f152fe07b3f87ed5bf8
edc <147658033+EstDC@users.noreply.github.com>
2023-10-20 13:19:54 +0200

    ejercicio4

--- ex1.py
+++ ex1.py
@@ -4,13 +4,19 @@ Exercices for input
 
 # 1. Ask the user for his/her name
 name = input("What is your name? ")
+print(name)
 
 # 2. Ask the user for his/her age
 age = float(input("what's your age?"))
+print(age)
 
 # 3. Ask the user for his/her favorite_color
+favorite_color= input("what's your favorite color?")
+print(favorite_color)
 
 # 4. Ask the user for his/her favorite_number
+favorite_number= input("what's your favorite number?")
+print(favorite_number)
 
 # 5. Print out the information in a sentence
 
